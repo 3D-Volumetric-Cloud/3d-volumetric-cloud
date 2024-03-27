e2c486e57ddff94dd06d86f5d74d96fa5ce2d3d0
lucvdacker <58031185+lucvdacker@users.noreply.github.com>
2024-03-27 14:19:55 +0100

    Homepage created

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -4,12 +4,11 @@ import Button from "@/components/Button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">Hello World!</h1>
-      <Button text="click me!!" clickHandler={() => {console.log('hi')}}/>
+    <main className="flex flex-col items-center p-24">
+      <h1 className="header-text">3D Volumetric Cloud Generation</h1>
+        <h3>Op FAA willen we blijven investeren in innovatieve technologische ontwikkelingen. Zo hebben we geïnvesteerd in verschillende technieken van Motion capture. Waar een nieuw project uit voort is gekomen; Volumetric Capture.</h3>
+        <h3>            Met technieken als photogrammetry en motion capture, kunnen we het digitaliseren van artiesten en performance kunstenaars nog meer documenteren en bewerken. Een artiest die meerdere instrumenten bespeelt zou in VR zijn eigen band kunnen bemensen in harmonie. Artiesten als Abba (abbatars) en Kiss (Rock Forever) zijn vereeuwigd met deze technieken. Een choreograaf zou zijn dansers los van zwaartekracht kunnen choreograferen. En ontwerpers zouden los van natuurkundige wetten en formaten nieuwe werkelijkheden kunnen creëren.</h3>
+        <h3>Tijdens de corona pandemie is er volop geëxperimenteerd met deze technology en een grote bijvangst van deze techniek was dat ook nieuwe podia, publiek en platform toegankelijk werden. “All kinds of Limbo” was een grote productie vanuit het National Theatre London, waar het stuk toegankelijk werd via AR, VR, Desktop – Tablet en nu nog steeds beschikbaar is.</h3>
     </main>
-      <div><h1>
-        Hello
-      </h1></div>
   );
 }
--- Frontend/src/components/NavBar/SearchBar.tsx
+++ Frontend/src/components/NavBar/SearchBar.tsx
@@ -1,7 +1,12 @@
 export default function SearchBar() {
     return (
-        <div className="rounded-lg bg-blue-900 min-w-60 min-h-8 flex justify-center">
-            <input className="text-blue-200 bg-blue-900 outline-none" type='text' placeholder="Search"/>
+        <div>
+            <a className="logo">
+                <img src='logo.png' alt='Official logo' width='100px' height='50px'/>
+            </a>
+            <div className="rounded-lg bg-blue-900 min-w-60 min-h-8 flex justify-center">
+                <input className="text-blue-200 bg-blue-900 outline-none" type='text' placeholder="Search"/>
+            </div>
         </div>
     );
 }
